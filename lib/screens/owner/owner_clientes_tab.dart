268a4bb7327b192693f855b8f90da68b262b271e
paulo960 <129551698+paulo960@users.noreply.github.com>
2026-08-26 15:17:29 -0300

    Update owner_clientes_tab.dart

--- lib/screens/owner/owner_clientes_tab.dart
+++ lib/screens/owner/owner_clientes_tab.dart
@@ -1,4 +1,19 @@
+// 1. Os Imports
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:intl/intl.dart';
 import 'dart:html' as html;
+
+class ClientesScreen extends StatefulWidget {
+  final String barbeariaId;
+  const ClientesScreen({super.key, required this.barbeariaId});
+
+  @override
+  State<ClientesScreen> createState() => _ClientesScreenState();
+}
+
+class _ClientesScreenState extends State<ClientesScreen> {
+  final TextEditingController _buscaCtrl = TextEditingController();
+  String _termoBusca = '';
+
+  
